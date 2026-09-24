0148c0253a3361ea38bfd03ebd827109e293d950
SirFizX <eeisaman@auburnschl.edu>
2026-09-24 09:54:30 -0400

    fix

--- apps/fetch_demo.py
+++ apps/fetch_demo.py
@@ -1,6 +1,6 @@
 # apps/fetch_demo.py - FIXED PARSE
 from scs import *
-from ./extensions/extensions.fetch import fetch_json, FetchError
+from extensions.fetch import fetch_json, FetchError
 from browser import aio
 import random
 
